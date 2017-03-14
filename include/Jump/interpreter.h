f939d3d3c455f871c0d2037165de531f13d2b5f2
andydev <akanshul97@gmail.com>
2017-03-14 01:53:43 -0400

    Implemented grammar parsing (up to expression)

--- include/Jump/interpreter.h
+++ include/Jump/interpreter.h
@@ -37,12 +37,20 @@ namespace Jump
      */
     class Interpreter {
     private:
+        /**
+         * Regex match constants
+         */
+        const std::regex_constants::match_flag_type REGEX_MATCH_CONSTANTS
+            = std::regex_constants::match_continuous;
+
         // ----------------------------- MATCHERS -----------------------------
-        // Words
+        // Keywords
         const std::regex CONSTANT   = std::regex("\\bconst\\b");
         const std::regex VARIABLE   = std::regex("\\bstate\\b");
         const std::regex STREAM     = std::regex("\\bstream\\b");
-        const std::regex PRINT      = std::regex("\\bread\\b");
+        const std::regex STATE      = std::regex("\\bstate\\b");
+        const std::regex READ       = std::regex("\\bread\\b");
+        const std::regex PRINT      = std::regex("\\bprint\\b");
         const std::regex TO         = std::regex("\\bto\\b");
         const std::regex LOOP       = std::regex("\\bloop\\b");
         const std::regex END        = std::regex("\\bend\\b");
@@ -51,16 +59,28 @@ namespace Jump
         const std::regex AND        = std::regex("\\band\\b");
         const std::regex OR         = std::regex("\\bor\\b");
         const std::regex NOT        = std::regex("\\bnot\\b");
+
+        // Values
+        const std::regex STRING     = std::regex(Values::STRING_REGEX);
+        const std::regex NUMBER     = std::regex(Values::NUMBER_REGEX);
         const std::regex BOOLEAN    = std::regex("\\b(True|False)\\b");
         const std::regex NULLVALUE  = std::regex("\\bNull\\b");
         const std::regex IDENTIFIER = std::regex("[a-zA-Z][a-zA-Z0-9_]*");
 
-        // Values
-        const std::regex STRING = std::regex(Values::STRING_REGEX);
-        const std::regex NUMBER = std::regex(Values::NUMBER_REGEX);
+        // StreamTypes
+        const std::regex ARRAYSTREAM = std::regex("ArrayStream");
 
         // Operations
-        const std::regex OPERATION = std::regex("[\\*\\+-/%=!><]+");
+        const std::regex ASSIGN    = std::regex("=");
+        const std::regex PIPE      = std::regex("->");
+        const std::regex GREATER   = std::regex(">");
+        const std::regex GREATEQ   = std::regex(">=");
+        const std::regex EQUAL     = std::regex("==");
+        const std::regex NOTEQUAL  = std::regex("!=");
+        const std::regex LESSEQ    = std::regex("<=");
+        const std::regex LESS      = std::regex("<");
+        const std::regex ADDSUB    = std::regex("+|-");
+        const std::regex MULDIVMOD = std::regex("*|/|%");
         const std::regex LPAREN    = std::regex("\\(");
         const std::regex RPAREN    = std::regex("\\)");
 
@@ -73,22 +93,37 @@ namespace Jump
         /**
          * The last input passed to the interpreter
          */
-        std::string m_input;
+        std::string& m_input;
 
         /**
          * The last token recieved
          */
-        std::string m_recieved;
+        std::string& m_recieved;
 
         /**
          * Last match results
          */
-        std::smatch m_matched;
+        std::smatch& m_matched;
 
         /**
          * The statemachine being built
          */
-        StateMachine machine;
+        StateMachine& m_machine;
+
+        /**
+         * The state being built
+         */
+        State* m_state;
+
+        /**
+         * The statement being built
+         */
+        Statements::Statement* m_statement;
+
+        /**
+         * The value being adjusted
+         */
+        Values::Value* m_value;
 
         /**
          * Returns true if the front supplies the given token
@@ -99,30 +134,60 @@ namespace Jump
          */
         bool recieve(std::regex reg);
 
+        /**
+         * Returns the last recieved token
+         *
+         * @return the last recieved token
+         */
+        std::string recieved();
+
+        /**
+         * Returns true if the string is at end
+         *
+         * @return true if the string is at end
+         */
+        bool end();
+
+        /**
+         * Reutrns the value stored in the statemachine at the given id
+         *
+         * @param id the id to store
+         *
+         * @return the value stored in the statemachine at the given id
+         */
+        Values::Value* get(std::string id) throw(Errors::ReferenceError);
+
+        /**
+         * Returns a peek at the first 5 characters of the input
+         *
+         * @return a peek at the first 5 characters of the input
+         */
+        std::string peek();
+
         // ----------------------------- NODES -----------------------------
-        void statemachine() throw(Errors::SyntaxError);
-        void constant() throw(Errors::SyntaxError);
-        void variable() throw(Errors::SyntaxError);
-        void stream() throw(Errors::SyntaxError);
-        void state() throw(Errors::SyntaxError);
-        void print() throw(Errors::SyntaxError);
-        void read() throw(Errors::SyntaxError);
-        void to() throw(Errors::SyntaxError);
-        void end() throw(Errors::SyntaxError);
-        void loop() throw(Errors::SyntaxError);
-        void feed() throw(Errors::SyntaxError);
-        void feedop() throw(Errors::SyntaxError);
-        void orr() throw(Errors::SyntaxError);
-        void orop() throw(Errors::SyntaxError);
-        void andd() throw(Errors::SyntaxError);
-        void andop() throw(Errors::SyntaxError);
-        void nott() throw(Errors::SyntaxError);
-        void compare() throw(Errors::SyntaxError);
-        void addsub() throw(Errors::SyntaxError);
-        void addsubOp() throw(Errors::SyntaxError);
-        void muldivmod() throw(Errors::SyntaxError);
-        void muldivmodOp() throw(Errors::SyntaxError);
-        void value() throw(Errors::SyntaxError);
+        void statemachine() throw(Errors::JumpError);
+        void constant() throw(Errors::JumpError);
+        void variable() throw(Errors::JumpError);
+        void stream() throw(Errors::JumpError);
+        void state() throw(Errors::JumpError);
+        void print() throw(Errors::JumpError);
+        void read() throw(Errors::JumpError);
+        void to() throw(Errors::JumpError);
+        void end() throw(Errors::JumpError);
+        void loop() throw(Errors::JumpError);
+        void feed() throw(Errors::JumpError);
+        void feedop() throw(Errors::JumpError);
+        void orr() throw(Errors::JumpError);
+        void orop() throw(Errors::JumpError);
+        void andd() throw(Errors::JumpError);
+        void andop() throw(Errors::JumpError);
+        void nott() throw(Errors::JumpError);
+        void compare() throw(Errors::JumpError);
+        void addsub() throw(Errors::JumpError);
+        void addsubOp() throw(Errors::JumpError);
+        void muldivmod() throw(Errors::JumpError);
+        void muldivmodOp() throw(Errors::JumpError);
+        void value() throw(Errors::JumpError);
     public:
         /**
          * Constructor for Interpreter
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -9,4 +9,319 @@ Created: 7 - 15 - 2016
 
 */
 
+// Headers being used
 #include "Jump/interpreter.h"
+#include "Jump/Statements/print.h"
+#include "Jump/Statements/read.h"
+#include "Jump/Statements/loop.h"
+#include "Jump/Statements/end.h"
+#include "Jump/Statements/to.h"
+#include "Jump/Values/string.h"
+#include "Jump/Values/null.h"
+#include "Jump/Values/boolean.h"
+#include "Jump/Values/identifier.h"
+#include "Jump/Values/expression.h"
+#include "Jump/Values/Numbers/unsignedinteger.h"
+#include "Jump/Values/Numbers/integer.h"
+#include "Jump/Values/Numbers/float.h"
+#include "Jump/Values/evaluate.h"
+#include "Jump/Streams/printstream.h"
+#include "Jump/Streams/readstream.h"
+#include "Jump/Streams/arraystream.h"
+
+// Libraries being used
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+// Namespaces being used
+using namespace std;
+using namespace Jump::Streams;
+using namespace Jump::Values;
+using namespace Jump::Errors;
+
+/**
+ * Jump is a new programming language that uses the state machine paradigm
+ *
+ * @author  Anshul Kharbanda
+ * @created 7 - 15 - 2016
+ */
+namespace Jump {
+    /**
+     * Interprets the string input as jump code and returns a StateMachine
+     *
+     * @param input the jump code to interpret
+     *
+     * @return the parsed StateMachine
+     *
+     * @throws JumpError if input is invalid jump code
+     */
+    StateMachine Interpreter::interpret(string input) throw(JumpError) {
+        // Recieve input
+        m_input = input;
+
+        // Create state machine
+        m_machine = StateMachine();
+        m_machine.setStream("stdin",  new ReadStream(cin));
+        m_machine.setStream("stdout", new PrintStream(cout));
+        m_machine.setStream("stderr", new PrintStream(cerr));
+        m_machine.setStream("prompt", new PrintStream(cout, " "));
+
+        // Start parser
+        statemachine();
+
+        // Return completed machine
+        return m_machine;
+    }
+
+    /**
+     * Returns the last recieved token
+     *
+     * @return the last recieved token
+     */
+    string Interpreter::recieved() {
+        return m_recieved;
+    }
+
+    /**
+     * Returns true if the string is at end
+     *
+     * @return true if the string is at end
+     */
+    bool Interpreter::end() {
+        return m_input.length() == 0;
+    }
+
+    /**
+     * Returns true if the front supplies the given token
+     *
+     * @param reg represents the token being recieved
+     *
+     * @return true if the front supplies the given token
+     */
+    bool Interpreter::recieve(regex reg) {
+        // Eliminate whitespaces
+        bool search = regex_search(m_input, m_matched, WSPACE, REGEX_MATCH_CONSTANTS);
+        if (search) input.erase(0, m_matched.length());
+
+        // Search
+        bool search = regex_search(m_input, m_matched, reg, REGEX_MATCH_CONSTANTS);
+        if (search) {
+            m_recieved = m_matched.str();
+            m_input.erase(0, m_matched.length());
+        }
+        return search;
+    }
+
+    /**
+     * Reutrns the value stored in the statemachine at the given id
+     *
+     * @param id the id to store
+     *
+     * @return the value stored in the statemachine at the given id
+     *
+     * @throw ReferenceError if value is not found in either const or var
+     */
+    Value* Interpreter::get(std::string id) throw(ReferenceError) {
+        try {
+            m_machine.constGet(id)
+        } catch (ReferenceError e) {
+            m_machine.varGet(id);
+        }
+    }
+
+    /**
+     * Returns a peek at the first 5 characters of the input
+     *
+     * @return a peek at the first 5 characters of the input
+     */
+    string peek() {
+        if (m_input.length() > 5) return m_input.substr(0,5) + "...";
+        else return m_input;
+    }
+
+    // ----------------------------- NODES -----------------------------
+    void Interpreter::statemachine() throw(JumpError) {
+        while(!end()) {
+            if (recieve(CONSTANT)) constant();
+            else if (recieve(VARIABLE)) variable();
+            else if (recieve(STREAM)) stream();
+            else if (recieve(STATE)) state();
+            else if (recieve(ENDLINE)) continue;
+            else throw SyntaxError("Unexpected token " + peek() + " Expected const, var, stream, state, or ENDLINE");
+        }
+    }
+
+    void Interpreter::constant() throw(JumpError) {
+        string id;
+        if (recieve(IDENTIFIER)) {
+            id = recieved();
+            if (recieved(ASSIGN)) m_machine->constSet(id, feed());
+            else throw SyntaxError("Unexpected token " + peek() + " Expected = after IDENTIFIER");
+        }
+        else throw SyntaxError("Unexpected token " + peek() + " Expected IDENTIFIER after const");
+        recieve(ENDLINE);
+    }
+
+    void Interpreter::variable() throw(JumpError) {
+        string id;
+        if (recieve(IDENTIFIER)) {
+            id = recieved();
+            if (recieved(ASSIGN)) m_machine->varSet(id, feed());
+            else throw SyntaxError("Unexpected token " + peek() + " Expected = after IDENTIFIER");
+        }
+        else throw SyntaxError("Unexpected token " + peek() + " Expected IDENTIFIER after var");
+        recieve(ENDLINE);
+    }
+
+    void Interpreter::stream() throw(JumpError) {
+        string id, stype;
+        if (recieve(IDENTIFIER)) {
+            id = recieved();
+            if (recieve(ASSIGN)) {
+                if (recieve(ARRAYSTREAM)) m_machine->streamSet(id, new ArrayStream());
+                else throw SyntaxError("Unexpected token " + peek() + " Expected STREAMTYPE after =");
+            }
+            else throw SyntaxError("Unexpected token " + peek() + " Expected = after IDENTIFIER");
+        }
+        else throw SyntaxError("Unexpected token " + peek() + " Expected IDENTIFIER after stream");
+    }
+
+    void Interpreter::state() throw(JumpError) {
+        string id;
+        if (recieve(IDENTIFIER)) {
+            id = recieved();
+            if (recieve(ENDLINE)) {
+                m_state = new State(id);
+                while(!end()) {
+                    if (recieve(READ)) read();
+                    else if (recieve(PRINT)) print();
+                    else if (recieve(TO)) to();
+                    else if (recieve(LOOP)) loop();
+                    else if (recieve(END)) end();
+                    else if (recieve(STATE)
+                          || recieve(CONSTANT)
+                          || recieve(VARIABLE)
+                          || recieve(STREAM)) break;
+                    else feed();
+                }
+                m_machine.stateSet(m_state);
+            }
+            else throw SyntaxError("Unexpected token " + peek() + " expected ENDLINE after state definition");
+        }
+        else throw SyntaxError("Unexpected token " + peek() + " expected IDENTIFIER after state");
+    }
+
+    void Interpreter::print() throw(JumpError) {
+        feed();
+        string id = "stdout";
+        if (recieve(PIPE)) {
+            if (recieve(IDENTIFIER)) {
+                id = recieved();
+            } else throw SyntaxError("Unexpected token " + peek() + " expected IDENTIFIER after ->");
+        }
+        m_state->add(new Print(m_value, id));
+    }
+
+    void Interpreter::read() throw(JumpError) {
+        string id1, id2;
+        if (recieve(IDENTIFIER)) {
+            id1 = recieved();
+        } else throw SyntaxError("Unexpected token " + peek() + " expected IDENTIFIER after read");
+
+        if (recieve(PIPE)) {
+            if (recieve(IDENTIFIER)) {
+                id2 = recieved();
+                m_state->add(new Read(new Identifier(id2), id1));
+            }
+        } else m_state->add(new Read(new Identifier(id1)));
+    }
+
+    void Interpreter::to() throw(JumpError) {
+        string id;
+        if (recieve(IDENTIFIER)) {
+            id = recieved();
+            if (recieve(IF)) {
+                feed();
+                m_state->add(new To(id, m_value));
+            } else {
+                recieve(OTHERWISE);
+                m_state->add(new To(id));
+            }
+        } else throw SyntaxError("Unexpected token " + peek() + " expected IDENTIFIER after to");
+    }
+
+    void Interpreter::end() throw(JumpError) {
+        if (recieve(IF)) {
+            feed();
+            m_state->add(new End(m_value));
+        } else {
+            recieve(OTHERWISE);
+            m_state->add(new End(new Boolean(true)));
+        }
+    }
+
+    void Interpreter::loop() throw(JumpError) {
+        if (recieve(IF)) {
+            feed();
+            m_state->add(new Loop(m_value));
+        } else {
+            recieve(OTHERWISE);
+            m_state->add(new Loop(new Boolean(true)));
+        }
+    }
+
+    // ------------------------------ EXPRESSION ------------------------------
+
+    void Interpreter::feed() throw(JumpError) {
+
+    }
+
+    void Interpreter::feedop() throw(JumpError) {
+
+    }
+
+    void Interpreter::orr() throw(JumpError) {
+
+    }
+
+    void Interpreter::orop() throw(JumpError) {
+
+    }
+
+    void Interpreter::andd() throw(JumpError) {
+
+    }
+
+    void Interpreter::andop() throw(JumpError) {
+
+    }
+
+    void Interpreter::nott() throw(JumpError) {
+
+    }
+
+    void Interpreter::compare() throw(JumpError) {
+
+    }
+
+    void Interpreter::addsub() throw(JumpError) {
+
+    }
+
+    void Interpreter::addsubOp() throw(JumpError) {
+
+    }
+
+    void Interpreter::muldivmod() throw(JumpError) {
+
+    }
+
+    void Interpreter::muldivmodOp() throw(JumpError) {
+
+    }
+
+    void Interpreter::value() throw(JumpError) {
+
+    }
+}
